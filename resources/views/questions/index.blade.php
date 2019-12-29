ce1aa9d6905965adfec84dc64af43ac79696632f
unknown <shahrozahmad786>
2019-12-29 16:31:57 +0500

    Building Question Controller and use pagination

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Questions</div>
+
+                <div class="card-body">
+                    
+                      
+                      @foreach ($questions as $question)
+                      <div class="media">
+                          <div class="media-body">
+                            <h3 class="mt-0">{{$question->title}}</h3>
+                            {{$question->body}}
+                          </div>
+                      </div>
+                      <hr>  
+                         
+                          
+                      @endforeach
+                      <div class="justify-c ontent-center">
+                         {{$questions->links()}}
+                      </div>
+                      
+                  
+
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
