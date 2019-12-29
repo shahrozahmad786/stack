310420b0ae997ca94654fb148fdfb1cb3555986b
unknown <shahrozahmad786>
2019-12-29 14:00:55 +0500

    Make Question Model Factory and use commands

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(App\Question::class, function (Faker $faker) {
+    return [
+            'title'=> rtrim($faker->sentence(rand(5,10)),"."),
+            'body'=>paragraphs(rand(2,3),true),
+            'views'=>rand(0,10),
+            'answers'=>rand(0,10),
+            'votes'=>rand(-5,10),
+             
+
+
+
+    ];
+});
