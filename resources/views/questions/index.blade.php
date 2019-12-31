05d3eb06ebdc55383b49cf4b063b59a5e7b80d9a
unknown <shahrozahmad786>
2019-12-31 20:11:43 +0500

    Class status and accessors added

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -14,12 +14,13 @@
                       <div class="media">
 
                         <div class="d-flex flex-column counters">
+                          
                           <div class="vote">
                           <strong>{{$question->votes}}</strong>
                           {{str_plural('vote',$question->votes)}}
                           </div>
 
-                          <div class="status">
+                          <div class="status {{$question->status}}">
                           <strong>{{$question->answers}}</strong>
                           {{str_plural('answer',$question->answers)}}
                           </div>
