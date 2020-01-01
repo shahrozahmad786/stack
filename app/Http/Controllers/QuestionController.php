673d681cc49e619b6d94cf2a746f424b52c98ec7
unknown <shahrozahmad786>
2020-01-01 14:45:09 +0500

    Delete Functionalityy has been implemented

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -96,6 +96,8 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+
+        return redirect()->route('questions.index')->with('success','Your Question has been deleted successfully');
     }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -57,6 +57,23 @@
 
                               <div class="ml-auto">
                                 <a href="{{ route('questions.edit',$question->id)}}" class="btn btn-sm btn-outline-info">Edit</a>
+              
+                  <form class="form-delete"  action="{{route('questions.destroy',$question->id)}}" method="post">
+              @method('Delete')
+              @csrf
+              <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete  </button>
+
+
+
+
+
+
+                                  
+
+                                </form>
+          
+            
+
                               </div>
                             </div>
                   
