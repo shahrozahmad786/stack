273564d6a1a5371f4e3fea17e6ebc86c62f08128
unknown <shahrozahmad786>
2020-01-02 19:40:53 +0500

    Fake Data Created

--- app/Answer.php
+++ app/Answer.php
@@ -11,18 +11,29 @@ class Answer extends Model
     
    public function question()
    {
-   	  return $this->belongTo(Question::class);
+   	  return $this->belongsTo(Question::class);
 
    }
 
 
    public function user()
    {
-   	  return $this->belongTo(User::class);
+   	  return $this->belongsTo(User::class);
 
    }
      public function getBodyHtmlAttribute()
      {
         return \Parsedown::instance()->text($this->body);
+     }
+
+     public static function boot()
+     {
+        parent::boot();
+
+        static::created(function($answer){
+         $answer->question->increment('answers_count');
+        });
+
+        
      }
 }
---
+++ database/factories/AnswerFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(App\Answer::class, function (Faker $faker) {
+    return [
+
+    	'body'=>$faker->paragraphs(rand(3,5),true),
+    	'user_id'=>App\User::pluck('id')->random(),
+    	'votes_count'=>rand(0,5)
+
+        
+
+    ];
+});
